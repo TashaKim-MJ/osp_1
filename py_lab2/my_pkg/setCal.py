0c8096b2e3b7707d50cdf847935ab4a1d987d993
TashaKim-MJ <mijookim.tasha@gmail.com>
2020-04-27 22:38:05 +0900

    union, intersection 출력에 작은따옴표 제거

--- py_lab2/my_pkg/setCal.py
+++ py_lab2/my_pkg/setCal.py
@@ -5,19 +5,16 @@ def setcal():
     
     temp1 = input("1st list: ")
     temp2 = input("2nd list: ")
-    print(type(temp1))
 
-    temp1.replace(" ", "")
-    print(temp1)
-    temp2.replace(" ", "")
     temp1 = re.sub('[,\[\] ]', '', temp1)
     temp2 = re.sub('[,\[\] ]', '', temp2)
 
     list1 = list(temp1)
-    print(list1)
     list2 = list(temp2)
     union = list(set(list1) | set(list2))
+    uni = sorted(list(map(int, union)))
     intersection = list(set(list1) & set(list2))
-    print ("union " , union)
-    print ("intersection " , intersection)
+    ins = sorted(list(map(int, intersection)))
+    print ("union " , uni)
+    print ("intersection " , ins)
 
