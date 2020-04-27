31b29d262baac7c707a78902a4880023ecea2617
TashaKim-MJ <mijookim.tasha@gmail.com>
2020-04-27 22:31:56 +0900

    3.xx 버전에 맞게 코드 수정

--- py_lab2/my_pkg/setCal.py
+++ py_lab2/my_pkg/setCal.py
@@ -5,11 +5,19 @@ def setcal():
     
     temp1 = input("1st list: ")
     temp2 = input("2nd list: ")
+    print(type(temp1))
+
+    temp1.replace(" ", "")
+    print(temp1)
+    temp2.replace(" ", "")
+    temp1 = re.sub('[,\[\] ]', '', temp1)
+    temp2 = re.sub('[,\[\] ]', '', temp2)
 
     list1 = list(temp1)
+    print(list1)
     list2 = list(temp2)
     union = list(set(list1) | set(list2))
     intersection = list(set(list1) & set(list2))
-    print "union " , union
-    print "intersection " , intersection
+    print ("union " , union)
+    print ("intersection " , intersection)
 
